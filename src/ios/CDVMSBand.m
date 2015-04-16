302e8b1c3a15aa9a98d25c199add4520cdcd8731
Jesse MacFadyen <purplecabbage@gmail.com>
2015-04-15 17:59:30 -0700

    exposed connect+queryVersionInfo

--- src/ios/CDVMSBand.m
+++ src/ios/CDVMSBand.m
@@ -8,52 +8,86 @@
 
 @implementation MSBandPlugin
 
--(void)initialize
+-(void)connect:(CDVInvokedUrlCommand*)command
 {
-//	[[MSBClientManager sharedManager] setDelegate:self];
-//	NSArray *attachedClients = [[MSBClientManager sharedManager] attachedClients];
-//
-//	MSBClient *client = [attachedClients firstObject];
-//	if (client)
-//	{
-//		[[MSBClientManager sharedManager] connectClient:client];
-//	}
+	[[MSBClientManager sharedManager] setDelegate:self];
+	NSArray *attachedClients = [[MSBClientManager sharedManager] attachedClients];
+
+	_client = [attachedClients firstObject];
+	if (_client)
+	{
+        // connect to it
+		[[MSBClientManager sharedManager] connectClient:_client];
+	}
+    else {
+        
+        // TODO: no bands detected
+    }
 }
 
 // Note: The delegate methods of MSBClientManagerDelegate protocol are called in the main thread.
 -(void)clientManager:(MSBClientManager *)cm clientDidConnect:(MSBClient *)client
 {
-// handle connected event.
+    // handle connected event.
+    self.client = client;
+    // TODO: notify js
 }
 
 -(void)clientManager:(MSBClientManager *)cm clientDidDisconnect:(MSBClient *)client
 {
 	// handle disconnected event.
+    self.client = nil;
 }
 
 -(void)clientManager:(MSBClientManager *)cm client:(MSBClient *)client didFailToConnectWithError:(NSError *)error
 {
 	// handle failure event.
+    // TODO: notify js
 }
 
 
 - (void)queryVersionInfo:(CDVInvokedUrlCommand*)command 
 {
+    __weak MSBClient* weakClient = self.client;
+    __block NSString* callbackId = [NSString stringWithString:command.callbackId ];
+    
 	// gets firmwareVersion and hardwareVersion
-// 	[self.client firmwareVersionWithCompletionHandler:^(NSString *version,
-// NSError *error){
-// if (error)
-//  // handle error
-// else
-//  // handle success
-// }];
-// [self.client hardwareVersionWithCompletionHandler:^(NSString *version,
-// NSError *error){
-// if (error)
-//  // handle error
-// else
-//  // handle success
-// }];
+ 	[weakClient firmwareVersionWithCompletionHandler:^(NSString *version,
+        NSError *error){
+        // create a dictionary to hold our results
+        NSMutableDictionary* versionInfo = [NSMutableDictionary dictionaryWithCapacity:2];
+        if (error) {
+            // handle error
+            dispatch_async(dispatch_get_main_queue(), ^{
+                CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+            });
+        }
+        else {
+            // handle success
+            // set the frameworkVersion, and go get the hardware version
+            [versionInfo setObject:[NSString stringWithString:version] forKey:@"frameworkVersion"];
+            [weakClient hardwareVersionWithCompletionHandler:^(NSString *hwVersion,                                                NSError *error){
+                if (error) {
+                    // handle error
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+                        [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+                    });
+                }
+                else {
+                    // handle success
+                    // got the hardware version
+                    [versionInfo setObject:[NSString stringWithString:hwVersion] forKey:@"hardwareVersion"];
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:versionInfo];
+                        
+                        [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+                    });
+                }
+            }];
+        }
+    }];
 }
 
 - (void)watchSensor:(CDVInvokedUrlCommand*)command
