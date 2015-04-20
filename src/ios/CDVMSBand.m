847f7fb72cf2ea5d95f3a03b79db92eb4c7f456a
Jesse MacFadyen <purplecabbage@gmail.com>
2015-04-20 15:27:24 -0700

    added the rest of the sensors

--- src/ios/CDVMSBand.m
+++ src/ios/CDVMSBand.m
@@ -106,28 +106,44 @@
 
 - (void)watchSensor:(CDVInvokedUrlCommand*)command
 {
+    // calories is a thing too
+//
+//            [self.client.sensorManager startCaloriesUpdatesToQueue:<#(NSOperationQueue *)#> errorRef:<#(NSError *__autoreleasing *)#> withHandler:<#^(MSBSensorCaloriesData *caloriesData, NSError *error)handler#>
+    
     __block NSString* callbackId = [NSString stringWithString:command.callbackId ];
     __block NSString* event = [NSString stringWithString:(NSString*)command.arguments[0]];
     
-    
     if([event compare:@"bandcontact"] == 0) {
 
+        [self.client.sensorManager startBandContactUpdatesToQueue:nil errorRef:nil withHandler:^(MSBSensorBandContactData *contactData, NSError *error) {
+            NSMutableDictionary* reading = [NSMutableDictionary dictionaryWithCapacity:4];
+            
+            [reading setValue:[NSNumber numberWithInt:contactData.wornState] forKey:@"wornState"];
+            [reading setValue:[NSNumber numberWithDouble:([[NSDate date] timeIntervalSince1970] * 1000)] forKey:@"timestamp"];
+            
+            NSMutableDictionary* payload = [NSMutableDictionary dictionaryWithCapacity:4];
+            [payload setValue:event forKey:@"event"];
+            [payload setValue:reading forKey:@"reading"];
+            
+            CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:payload];
+            [result setKeepCallback:[NSNumber numberWithBool:YES]];
+            [self.commandDelegate sendPluginResult:result callbackId:callbackId];
+        }];
+        
     }
     else if([event compare:@"accelerometer"] == 0) {
         [self.client.sensorManager startAccelerometerUpdatesToQueue:nil errorRef:nil withHandler:^(MSBSensorAccelData *accelerometerData, NSError *error) {
             
+            NSMutableDictionary* reading = [NSMutableDictionary dictionaryWithCapacity:4];
             
-            NSMutableDictionary* accelProps = [NSMutableDictionary dictionaryWithCapacity:4];
-            
-            [accelProps setValue:[NSNumber numberWithDouble:accelerometerData.x] forKey:@"x"];
-            [accelProps setValue:[NSNumber numberWithDouble:accelerometerData.y] forKey:@"y"];
-            [accelProps setValue:[NSNumber numberWithDouble:accelerometerData.z] forKey:@"z"];
-            [accelProps setValue:[NSNumber numberWithDouble:([[NSDate date] timeIntervalSince1970] * 1000)] forKey:@"timestamp"];
+            [reading setValue:[NSNumber numberWithDouble:accelerometerData.x] forKey:@"x"];
+            [reading setValue:[NSNumber numberWithDouble:accelerometerData.y] forKey:@"y"];
+            [reading setValue:[NSNumber numberWithDouble:accelerometerData.z] forKey:@"z"];
+            [reading setValue:[NSNumber numberWithDouble:([[NSDate date] timeIntervalSince1970] * 1000)] forKey:@"timestamp"];
             
             NSMutableDictionary* payload = [NSMutableDictionary dictionaryWithCapacity:4];
             [payload setValue:event forKey:@"event"];
-            [payload setValue:accelProps forKey:@"reading"];
-            
+            [payload setValue:reading forKey:@"reading"];
             
             
             CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:payload];
@@ -137,32 +153,155 @@
         
     }
     else if([event compare:@"gyroscope"] == 0) {
+        [self.client.sensorManager startGyroscopeUpdatesToQueue:nil errorRef:nil withHandler:^(MSBSensorGyroData *accelGyroData, NSError *error) {
+            
+            NSMutableDictionary* reading = [NSMutableDictionary dictionaryWithCapacity:4];
+            
+            [reading setValue:[NSNumber numberWithDouble:accelGyroData.x] forKey:@"x"];
+            [reading setValue:[NSNumber numberWithDouble:accelGyroData.y] forKey:@"y"];
+            [reading setValue:[NSNumber numberWithDouble:accelGyroData.z] forKey:@"z"];
+            [reading setValue:[NSNumber numberWithDouble:([[NSDate date] timeIntervalSince1970] * 1000)] forKey:@"timestamp"];
+            
+            NSMutableDictionary* payload = [NSMutableDictionary dictionaryWithCapacity:2];
+            [payload setValue:event forKey:@"event"];
+            [payload setValue:reading forKey:@"reading"];
+            
+            
+            CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:payload];
+            [result setKeepCallback:[NSNumber numberWithBool:YES]];
+            [self.commandDelegate sendPluginResult:result callbackId:callbackId];
+        }];
         
     }
     else if([event compare:@"distance"] == 0) {
-        
+        [self.client.sensorManager startDistanceUpdatesToQueue:nil errorRef:nil withHandler:^(MSBSensorDistanceData *distanceData, NSError *error) {
+            
+            NSMutableDictionary* reading = [NSMutableDictionary dictionaryWithCapacity:5];
+            
+            [reading setValue:[NSNumber numberWithDouble:distanceData.totalDistance] forKey:@"totalDistance"];
+            [reading setValue:[NSNumber numberWithDouble:distanceData.speed] forKey:@"speed"];
+            [reading setValue:[NSNumber numberWithDouble:distanceData.pace] forKey:@"pace"];
+            [reading setValue:[NSNumber numberWithInt:distanceData.pedometerMode ]forKey:@"pedometerMode"];
+            [reading setValue:[NSNumber numberWithDouble:([[NSDate date] timeIntervalSince1970] * 1000)] forKey:@"timestamp"];
+            
+            NSMutableDictionary* payload = [NSMutableDictionary dictionaryWithCapacity:2];
+            [payload setValue:event forKey:@"event"];
+            [payload setValue:reading forKey:@"reading"];
+            
+            CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:payload];
+            [result setKeepCallback:[NSNumber numberWithBool:YES]];
+            [self.commandDelegate sendPluginResult:result callbackId:callbackId];
+
+        }];
+     
     }
     else if([event compare:@"heartrate"] == 0) {
-        
+        [self.client.sensorManager startHearRateUpdatesToQueue:nil errorRef:nil withHandler:^(MSBSensorHeartRateData *heartRateData, NSError *error) {
+            //heartRate,quality
+            NSMutableDictionary* reading = [NSMutableDictionary dictionaryWithCapacity:3];
+            
+            [reading setValue:[NSNumber numberWithDouble:heartRateData.heartRate] forKey:@"heartRate"];
+            [reading setValue:[NSNumber numberWithInt:heartRateData.quality] forKey:@"quality"];
+            [reading setValue:[NSNumber numberWithDouble:([[NSDate date] timeIntervalSince1970] * 1000)] forKey:@"timestamp"];
+            
+            NSMutableDictionary* payload = [NSMutableDictionary dictionaryWithCapacity:2];
+            [payload setValue:event forKey:@"event"];
+            [payload setValue:reading forKey:@"reading"];
+            
+            CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:payload];
+            [result setKeepCallback:[NSNumber numberWithBool:YES]];
+            [self.commandDelegate sendPluginResult:result callbackId:callbackId];
+        }];
     }
     else if([event compare:@"pedometer"] == 0) {
-        
+        [self.client.sensorManager startPedometerUpdatesToQueue:nil errorRef:nil withHandler:^(MSBSensorPedometerData *pedometerData, NSError *error) {
+            //totalSteps, stepRate, movementRate, totalMovements, movementMode
+            NSMutableDictionary* reading = [NSMutableDictionary dictionaryWithCapacity:6];
+            
+            [reading setValue:[NSNumber numberWithInt:pedometerData.totalSteps] forKey:@"totalSteps"];
+            [reading setValue:[NSNumber numberWithDouble:pedometerData.stepRate] forKey:@"stepRate"];
+            [reading setValue:[NSNumber numberWithDouble:pedometerData.movementRate] forKey:@"movementRate"];
+            [reading setValue:[NSNumber numberWithInt:pedometerData.totalMovements ]forKey:@"totalMovements"];
+            [reading setValue:[NSNumber numberWithInt:pedometerData.movementMode ]forKey:@"movementMode"];
+            
+            [reading setValue:[NSNumber numberWithDouble:([[NSDate date] timeIntervalSince1970] * 1000)] forKey:@"timestamp"];
+            
+            NSMutableDictionary* payload = [NSMutableDictionary dictionaryWithCapacity:2];
+            [payload setValue:event forKey:@"event"];
+            [payload setValue:reading forKey:@"reading"];
+            
+            CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:payload];
+            [result setKeepCallback:[NSNumber numberWithBool:YES]];
+            [self.commandDelegate sendPluginResult:result callbackId:callbackId];
+        }];
     }
     else if([event compare:@"skintemperature"] == 0) {
-        
+        [self.client.sensorManager startSkinTempUpdatesToQueue:nil errorRef:nil withHandler:^(MSBSensorSkinTempData *skinTempData, NSError *error) {
+            //temperature
+            NSMutableDictionary* reading = [NSMutableDictionary dictionaryWithCapacity:2];
+            
+            [reading setValue:[NSNumber numberWithDouble:skinTempData.temperature] forKey:@"temperature"];
+            [reading setValue:[NSNumber numberWithDouble:([[NSDate date] timeIntervalSince1970] * 1000)] forKey:@"timestamp"];
+            
+            NSMutableDictionary* payload = [NSMutableDictionary dictionaryWithCapacity:2];
+            [payload setValue:event forKey:@"event"];
+            [payload setValue:reading forKey:@"reading"];
+            
+            CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:payload];
+            [result setKeepCallback:[NSNumber numberWithBool:YES]];
+            [self.commandDelegate sendPluginResult:result callbackId:callbackId];
+        }];
     }
     else if([event compare:@"uvlevel"] == 0) {
-        
+        [self.client.sensorManager startUVUpdatesToQueue:nil errorRef:nil withHandler:^(MSBSensorUVData *UVData, NSError *error) {
+            //uvIndexLevel [ MSBUVLevelNone, MSBUVLevelLow, MSBUVLevelMedium, MSBUVLevelHigh, MSBUVLevelVeryHigh ]
+            NSMutableDictionary* reading = [NSMutableDictionary dictionaryWithCapacity:2];
+            
+            [reading setValue:[NSNumber numberWithDouble:UVData.uvIndexLevel] forKey:@"uvIndexLevel"];
+            [reading setValue:[NSNumber numberWithDouble:([[NSDate date] timeIntervalSince1970] * 1000)] forKey:@"timestamp"];
+            
+            NSMutableDictionary* payload = [NSMutableDictionary dictionaryWithCapacity:2];
+            [payload setValue:event forKey:@"event"];
+            [payload setValue:reading forKey:@"reading"];
+            
+            CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:payload];
+            [result setKeepCallback:[NSNumber numberWithBool:YES]];
+            [self.commandDelegate sendPluginResult:result callbackId:callbackId];
+            
+        }];
     }
-//    dispatch_async(dispatch_get_main_queue(), ^{
-//        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-//        [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
-//    });
 }
 
 - (void)unwatchSensor:(CDVInvokedUrlCommand*)command
 {
     __block NSString* callbackId = [NSString stringWithString:command.callbackId ];
+    __block NSString* event = [NSString stringWithString:(NSString*)command.arguments[0]];
+    
+    
+    if([event compare:@"bandcontact"] == 0) {
+        [self.client.sensorManager stopBandContactUpdatesErrorRef:nil];
+    }
+    else if([event compare:@"accelerometer"] == 0) {
+        [self.client.sensorManager stopAccelerometerUpdatesErrorRef:nil];
+    }
+    else if([event compare:@"gyroscope"] == 0) {
+        [self.client.sensorManager stopGyroscopeUpdatesErrorRef:nil];
+    }
+    else if([event compare:@"distance"] == 0) {
+        [self.client.sensorManager stopDistanceUpdatesErrorRef:nil];
+    }
+    else if([event compare:@"heartrate"] == 0) {
+        [self.client.sensorManager stopHeartRateUpdatesErrorRef:nil];
+    }
+    else if([event compare:@"pedometer"] == 0) {
+        [self.client.sensorManager stopPedometerUpdatesErrorRef:nil];
+    }
+    else if([event compare:@"skintemperature"] == 0) {
+        [self.client.sensorManager stopSkinTempUpdatesErrorRef:nil];
+    }
+    else if([event compare:@"uvlevel"] == 0) {
+        [self.client.sensorManager stopUVUpdatesErrorRef:nil];
+    }
     
     dispatch_async(dispatch_get_main_queue(), ^{
         CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
